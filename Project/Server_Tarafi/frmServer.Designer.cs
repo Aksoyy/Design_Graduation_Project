63a0cffbe9fe1262ad1fce98e75ae5923ca22994
Hakan Aksoy <hkn.aksoyy@gmail.com>
2019-05-31 01:38:32 +0300

    Add files via upload

---
+++ Project/Server_Tarafi/frmServer.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Server_Tarafi
+{
+    partial class frmServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.linkFilePath = new System.Windows.Forms.LinkLabel();
+            this.KeyGonder = new System.Windows.Forms.Button();
+            this.btOpenDialog = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGonderServer = new System.Windows.Forms.Button();
+            this.txtMesajServer = new System.Windows.Forms.TextBox();
+            this.lstMesajlarServer = new System.Windows.Forms.ListBox();
+            this.btnLogShow = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnLogShow);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.linkFilePath);
+            this.panel2.Controls.Add(this.KeyGonder);
+            this.panel2.Controls.Add(this.btOpenDialog);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnGonderServer);
+            this.panel2.Controls.Add(this.txtMesajServer);
+            this.panel2.Controls.Add(this.lstMesajlarServer);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(622, 396);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(446, 207);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // linkFilePath
+            // 
+            this.linkFilePath.AutoSize = true;
+            this.linkFilePath.Location = new System.Drawing.Point(454, 44);
+            this.linkFilePath.Name = "linkFilePath";
+            this.linkFilePath.Size = new System.Drawing.Size(59, 13);
+            this.linkFilePath.TabIndex = 13;
+            this.linkFilePath.TabStop = true;
+            this.linkFilePath.Text = "Dosya yolu";
+            this.linkFilePath.Visible = false;
+            this.linkFilePath.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkFilePath_LinkClicked);
+            // 
+            // KeyGonder
+            // 
+            this.KeyGonder.Location = new System.Drawing.Point(445, 233);
+            this.KeyGonder.Name = "KeyGonder";
+            this.KeyGonder.Size = new System.Drawing.Size(165, 23);
+            this.KeyGonder.TabIndex = 12;
+            this.KeyGonder.Text = "KeyGonder";
+            this.KeyGonder.UseVisualStyleBackColor = true;
+            this.KeyGonder.Click += new System.EventHandler(this.KeyGonder_Click);
+            // 
+            // btOpenDialog
+            // 
+            this.btOpenDialog.Location = new System.Drawing.Point(535, 39);
+            this.btOpenDialog.Name = "btOpenDialog";
+            this.btOpenDialog.Size = new System.Drawing.Size(75, 23);
+            this.btOpenDialog.TabIndex = 11;
+            this.btOpenDialog.Text = "Dosya Seç";
+            this.btOpenDialog.UseVisualStyleBackColor = true;
+            this.btOpenDialog.Visible = false;
+            this.btOpenDialog.Click += new System.EventHandler(this.BtOpenDialog_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "PlainText",
+            "Crypto1",
+            "Crypto2",
+            "Crypto3"});
+            this.comboBox1.Location = new System.Drawing.Point(445, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(165, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "Şifreleme Yöntemi Seçiniz";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(105, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(13, 370);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Gelen Mesaj: ";
+            // 
+            // btnGonderServer
+            // 
+            this.btnGonderServer.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGonderServer.Location = new System.Drawing.Point(445, 262);
+            this.btnGonderServer.Name = "btnGonderServer";
+            this.btnGonderServer.Size = new System.Drawing.Size(165, 94);
+            this.btnGonderServer.TabIndex = 2;
+            this.btnGonderServer.Text = "Gönder";
+            this.btnGonderServer.UseVisualStyleBackColor = true;
+            this.btnGonderServer.Click += new System.EventHandler(this.BtnGonderServer_Click);
+            // 
+            // txtMesajServer
+            // 
+            this.txtMesajServer.Location = new System.Drawing.Point(13, 335);
+            this.txtMesajServer.Name = "txtMesajServer";
+            this.txtMesajServer.Size = new System.Drawing.Size(426, 20);
+            this.txtMesajServer.TabIndex = 1;
+            // 
+            // lstMesajlarServer
+            // 
+            this.lstMesajlarServer.FormattingEnabled = true;
+            this.lstMesajlarServer.Location = new System.Drawing.Point(12, 12);
+            this.lstMesajlarServer.Name = "lstMesajlarServer";
+            this.lstMesajlarServer.Size = new System.Drawing.Size(427, 316);
+            this.lstMesajlarServer.TabIndex = 0;
+            // 
+            // btnLogShow
+            // 
+            this.btnLogShow.Location = new System.Drawing.Point(445, 361);
+            this.btnLogShow.Name = "btnLogShow";
+            this.btnLogShow.Size = new System.Drawing.Size(165, 23);
+            this.btnLogShow.TabIndex = 15;
+            this.btnLogShow.Text = "Log göster";
+            this.btnLogShow.UseVisualStyleBackColor = true;
+            this.btnLogShow.Click += new System.EventHandler(this.btnLogShow_Click);
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(622, 396);
+            this.Controls.Add(this.panel2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(638, 435);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(638, 435);
+            this.Name = "frmServer";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.FrmServer_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox lstMesajlarServer;
+        private System.Windows.Forms.Button btnGonderServer;
+        private System.Windows.Forms.TextBox txtMesajServer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btOpenDialog;
+        private System.Windows.Forms.Button KeyGonder;
+        private System.Windows.Forms.LinkLabel linkFilePath;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnLogShow;
+    }
+}
+
---
+++ Project/Server_Tarafi/frmServer.cs
@@ -0,0 +1,329 @@
+﻿using CryptoLibrary;
+using LogHelperLib;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Server_Tarafi
+{
+    public partial class frmServer : Form
+    {
+        public frmServer()
+        {
+            InitializeComponent(); 
+            // Form açıkken enter'a basınca verinin gonderilmesidir.
+            this.AcceptButton = btnGonderServer;
+        }
+
+        TcpListener dinleyici;
+        Socket sckKullanici;
+        NetworkStream ns;
+        StreamWriter clientWriter;
+        BinaryReader binaryReader = new BinaryReader(new MemoryStream());
+        private StreamReader clientReader;
+        frmServerLog log;
+
+        private byte[] m_CipherByteArray;
+        private string m_ImageFilePath, dataPath;
+        private string connectName;
+        string simetricKey = "";
+        bool isSecretKey = false;
+
+        private void FrmServer_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0; //cbChoose.SelectedIndex = 0;
+            CheckForIllegalCrossThreadCalls = false;
+
+            dinleyici = new TcpListener(8082);
+            dinleyici.Start();
+
+            // Dinle metodu içerisinde sonsuz döngü olacağı için bu işlemi thread'e alındı.
+            Thread th = new Thread(Dinle);
+            th.Start();
+        }
+
+        void Dinle()
+        {
+            // Bağlanan kullanıcı sckKullanici isimli Socket nesnesine alındı.
+            sckKullanici = dinleyici.AcceptSocket();
+            LogWriter("Kullanıcı geldi");
+
+            // NetworkStream, ağ üzerine yazmak için kullanılır. 
+            ns = new NetworkStream(sckKullanici);
+
+            // Kullanıcıya mesaj göndermek ve almak için StreamWriter içe NetworkStream üzerine yazacağız.
+            clientWriter = new StreamWriter(ns, Encoding.ASCII);
+            clientReader = new StreamReader(ns, Encoding.ASCII);
+
+            while (true)
+            {
+                string clienttanGelenMesaj = clientReader.ReadLine();
+
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    Crypto1 crypto1 = new Crypto1();
+
+                    UnicodeEncoding ue = new UnicodeEncoding();
+                    Byte[] buffer = Convert.FromBase64String(clienttanGelenMesaj);
+
+                    byte[] plainText = crypto1.DecryptToByteArray(buffer, simetricKey);
+
+                    File.WriteAllBytes(dataPath, plainText);
+                    lstMesajlarServer.Items.Add("Desifreleme islemi tamamlandi");
+                    LogWriter("Desifreleme islemi tamamlandi");
+
+                    //File.WriteAllBytes(@"C:\Users\Hknaksoyy\Desktop\cipherText.jpg", plainText);
+                    //LinkLabelLinkClickedEventArgs ex = new LinkLabelLinkClickedEventArgs();
+                    //LinkFilePath_LinkClicked = true;
+                    //this.LinkFilePath_LinkClicked += this.LinkFilePath_LinkClicked;
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    if (!string.IsNullOrEmpty(clienttanGelenMesaj))
+                    {
+                        Crypto2 crypto2 = new Crypto2();
+                        string decryptoHash = clienttanGelenMesaj.Substring((clienttanGelenMesaj.Length - 64), 64);
+                        string data = clienttanGelenMesaj.Substring(0, (clienttanGelenMesaj.Length - 64));
+                        if ( ComputeSha256Hash(data) == decryptoHash )
+                        {
+                            lstMesajlarServer.Items.Add(connectName + ": " + crypto2.DecryptKey(data, simetricKey));
+                            label2.Text = clienttanGelenMesaj;
+                            LogWriter(clienttanGelenMesaj);
+                        }
+                    }
+                }
+                else if (comboBox1.SelectedIndex == 3)
+                {
+                    Crypto3 crypto3 = new Crypto3();
+                    lstMesajlarServer.Items.Add(connectName + ": " + crypto3.Decrypto(clienttanGelenMesaj));
+                    label2.Text = clienttanGelenMesaj;
+                    LogWriter(clienttanGelenMesaj);
+                }
+                // Acik metin şeklinde iletimi saglar.
+                else
+                {
+                    if (clienttanGelenMesaj.EndsWith(" isimli kullanici geldi."))
+                    {
+                        // Gelen kullanıcının tanımlanmasıdır.
+                        Console.WriteLine(clienttanGelenMesaj);
+                        connectName = clienttanGelenMesaj.Substring(0, clienttanGelenMesaj.Length - 24);
+                        lstMesajlarServer.Items.Add(clienttanGelenMesaj);
+                        LogWriter(clienttanGelenMesaj);
+                    }
+                    else
+                    {
+                        lstMesajlarServer.Items.Add(connectName + ": " + clienttanGelenMesaj);
+                        LogWriter(connectName + ": " + clienttanGelenMesaj);
+                    }
+                }
+            }
+        }
+   
+        private void BtnGonderServer_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            { 
+                if (!isSecretKey && !string.IsNullOrEmpty(txtMesajServer.Text))
+                {
+                    string publicKeyXML = String.Format(@"<RSAKeyValue>
+                                                               <Modulus>{0}</Modulus>
+                                                               <Exponent>{1}</Exponent>
+                                                            </RSAKeyValue>",
+                                                            ConfigurationSettings.AppSettings.Get("Modulus"),
+                                                            ConfigurationSettings.AppSettings.Get("Exponent"));
+
+                    string encryptSecretKey = Crypto2.EncryptSecretKey(txtMesajServer.Text, publicKeyXML);
+                    LogWriter("Sifrelenecek Mesaj : " + txtMesajServer.Text);
+                    LogWriter("Sifrelenmis Gizli Anahtar : " + encryptSecretKey);
+                    clientWriter.WriteLine(encryptSecretKey);
+                    clientWriter.Flush();
+                    isSecretKey = true;
+                }
+                else
+                {
+                    Crypto1 crypto1 = new Crypto1();
+                    byte[] acikMetin = File.ReadAllBytes(m_ImageFilePath);
+                    byte[] sifreliMetin = crypto1.EncryptToByteArray(acikMetin, simetricKey);
+
+                    clientWriter.WriteLine(/*"UPLOAD_START_" +*/ Convert.ToBase64String(sifreliMetin) /*+ "_UPLOAD_END"*/);
+                    clientWriter.Flush();
+
+                    lstMesajlarServer.Items.Add("Dosya yolu : " + m_ImageFilePath + " gönderildi.");
+                    LogWriter("Dosya yolu : " + m_ImageFilePath + " gönderildi.");
+                }
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                //Gizli anahtarın paylaşımını sağlar.
+                if (!isSecretKey && !string.IsNullOrEmpty(txtMesajServer.Text))
+                {
+                    string publicKeyXML = String.Format(@"<RSAKeyValue>
+                                                               <Modulus>{0}</Modulus>
+                                                               <Exponent>{1}</Exponent>
+                                                            </RSAKeyValue>",
+                                                            ConfigurationSettings.AppSettings.Get("Modulus"),
+                                                            ConfigurationSettings.AppSettings.Get("Exponent"));
+                    
+                    string encryptSecretKey = Crypto2.EncryptSecretKey(txtMesajServer.Text, publicKeyXML);
+                    LogWriter("Sifrelenecek Mesaj : " + txtMesajServer.Text);
+                    LogWriter("Sifrelenmis Gizli Anahtar : " + encryptSecretKey);
+                    clientWriter.WriteLine(encryptSecretKey);
+                    clientWriter.Flush();
+                    simetricKey = txtMesajServer.Text;
+                    isSecretKey = true;
+                }
+                else
+                {
+                    Crypto2 crypto2 = new Crypto2();
+                    string encrypted = crypto2.EncryptKey(txtMesajServer.Text,simetricKey);
+                    //GONDERILEN KISIM
+                    string encryptedHash = encrypted;
+                    encryptedHash += ComputeSha256Hash(encrypted);
+                    clientWriter.WriteLine(encryptedHash);
+                    clientWriter.Flush();
+                    //SERVER'IN KENDI LISTBOX'INA EKLEME ISLEMI
+                    lstMesajlarServer.Items.Add("Server: " + txtMesajServer.Text);
+                    LogWriter("Server: " + txtMesajServer.Text);
+                }
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                Crypto3 crypto3 = new Crypto3();
+                string encrypted = crypto3.Encrypto(txtMesajServer.Text);
+                //GONDERILEN KISIM
+                clientWriter.WriteLine(encrypted);
+                clientWriter.Flush();
+                //SERVER'IN KENDI LISTBOX'INA EKLEME ISLEMI
+                lstMesajlarServer.Items.Add("Server: " + txtMesajServer.Text);
+                LogWriter("Server: " + txtMesajServer.Text);
+            }
+            else
+            {
+                clientWriter.WriteLine(txtMesajServer.Text);
+                clientWriter.Flush();
+                lstMesajlarServer.Items.Add("Server: " + txtMesajServer.Text);
+                LogWriter("Server: " + txtMesajServer.Text);
+
+            }
+            //GONDERIM ISLEMINDEN SONRA TEXT BOLGESI TEMIZLENIR.
+            txtMesajServer.Clear();
+        }
+   
+        //Kullanıcının şifrelenecek yolu seçtiği kısımdır.
+        private void BtOpenDialog_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "image files (*.jpeg)|*.jpg";
+            dialog.InitialDirectory = @"C:\";
+            dialog.Title = "Please select an image file to encrypt.";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                m_ImageFilePath = dialog.FileName;
+            }
+        }
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = comboBox1.SelectedIndex;
+            if (index == 1 || index == 2)
+            { 
+                if (!isSecretKey)
+                {
+                    lstMesajlarServer.Items.Add("İletisim İcin Secret Key giriniz");
+                }
+            }
+            linkFilePath.Visible = index == 1;
+            btOpenDialog.Visible = index == 1;
+        }
+        private void KeyGonder_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                txtMesajServer.Text = textBox1.Text;
+            }
+            else
+            {
+                txtMesajServer.Text = "asdfgytasdfgytrfghtuytorfghtuyto";
+            }
+        }
+        private string CheckControl(string text)
+        {
+            string textArray = null;
+            if (!string.IsNullOrEmpty(text) && text.StartsWith("UPLOAD_START_"))
+            {
+                textArray = text.Substring(13, text.Length - 13);
+                lstMesajlarServer.Items.Add(textArray);
+
+                bool state = true;
+                while (state)
+                {
+                    string temp = clientReader.ReadLine();
+                    if (!string.IsNullOrEmpty(temp))
+                    {
+                        if (!temp.EndsWith("_UPLOAD_END"))
+                        {
+                            textArray += temp;
+                        }
+                        else
+                        {
+                            textArray += temp.Substring(0, temp.Length - 11);
+                            state = false;
+                        }
+                    }
+                }
+                m_CipherByteArray = Encoding.ASCII.GetBytes(textArray);
+                return null;
+            }
+            return text;
+        }
+        private void LinkFilePath_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            //dialog.Filter = "image files (*.jpeg)|*.jpg";
+            dialog.InitialDirectory = @"C:\";
+            dialog.Title = "Please select an image file to encrypt.";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                dataPath = Path.GetDirectoryName(dialog.FileName.ToString());
+                dataPath += "\\decrypto.jpg"; //m_ImageFilePath = dialog.FileName;
+            }
+        }
+
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private void btnLogShow_Click(object sender, EventArgs e)
+        {
+            log = new frmServerLog();
+            log.Show();
+        }
+
+        private void LogWriter(string msg)
+        {
+            LogHelper.LogWrite(msg);
+            if (log != null)
+                log.writeLog(msg);
+        }
+    }
+}
